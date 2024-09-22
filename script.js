403c6884bf87a784b297414a719571653e277ff6
Sahitya-Dwivedi <sahityadwivedi515@gmail.com>
2024-09-22 16:21:29 +0530

    code optimization

--- script.js
+++ script.js
@@ -1,146 +1,47 @@
-let box = document.querySelectorAll(".box");
-let check = document.querySelector("#check");
-let whoTurn = document.querySelector(".whoTurn");
-let checkComp = document.querySelector("#checkComp");
+const box = document.querySelectorAll(".box");
+const check = document.querySelector("#check");
+const whoTurn = document.querySelector(".whoTurn");
+const checkComp = document.querySelector("#checkComp");
+const X = document.querySelector(".X");
+const O = document.querySelector(".O");
 let toggle = true;
 let checked = false;
 let movecount = 0;
+
 whoTurn.textContent = "X";
 
 check.addEventListener("change", (e) => {
-  e.target.checked ? (checked = true) : ((checked = false), reset());
+  e.target.checked ? boxTapping() : reset();
 });
 checkComp.addEventListener("change", (e) => {
-  e.target.checked ? computer() : "";
+  e.target.checked ? computer() : reset();
 });
-
-boxTapping();
-
-function winner() {
-  if (
-    box[0].textContent == box[1].textContent &&
-    box[1].textContent == box[2].textContent &&
-    box[0].textContent != ""
-  ) {
-    if (box[0].textContent == "X") {
-      alert("X won");
-    } else {
-      alert("O won")
-    }
-    reset()
-    reset();
-  } else if (
-    box[3].textContent == box[4].textContent &&
-    box[4].textContent == box[5].textContent &&
-    box[3].textContent != ""
-  ) {
-    if (box[3].textContent == "X") {
-      alert("X won");
-    } else {
-      alert("O won")
-    }
-    reset();
-  } else if (
-    box[6].textContent == box[7].textContent &&
-    box[7].textContent == box[8].textContent &&
-    box[6].textContent != ""
-  ) {
-    if (box[6].textContent == "X") {
-      alert("X won");
-    } else {
-      alert("O won")
-    }
-    reset();
-  } else if (
-    box[0].textContent == box[3].textContent &&
-    box[3].textContent == box[6].textContent &&
-    box[0].textContent != ""
-  ) {
-    if (box[0].textContent == "X") {
-      alert("X won");
-    } else {
-      alert("O won")
-    }
-    reset();
-  } else if (
-    box[1].textContent == box[4].textContent &&
-    box[4].textContent == box[7].textContent &&
-    box[1].textContent != ""
-  ) {
-    if (box[1].textContent == "X") {
-      alert("X won");
-    } else {
-      alert("O won")
-    }
-    reset();
-  } else if (
-    box[2].textContent == box[5].textContent &&
-    box[5].textContent == box[8].textContent &&
-    box[2].textContent != ""
-  ) {
-    if (box[2].textContent == "X") {
-      alert("X won");
-    } else {
-      alert("O won")
-    }
-    reset();
-  } else if (
-    box[0].textContent == box[4].textContent &&
-    box[4].textContent == box[8].textContent &&
-    box[0].textContent != ""
-  ) {
-    if (box[0].textContent == "X") {
-      alert("X won");
-    } else {
-      alert("O won")
-    }
-    reset();
-  } else if (
-    box[2].textContent == box[4].textContent &&
-    box[4].textContent == box[6].textContent &&
-    box[2].textContent != ""
-  ) {
-    if (box[2].textContent == "X") {
-      alert("X won");
-    } else {
-      alert("O won")
-    }
-    reset();
-  }
-  let draw = 0;
-  box.forEach((v) => (v.textContent != "" ? draw++ : ""));
-  if (draw == 9) {
-    alert("draw");
-    reset();
-  }
-}
-
 function reset() {
   toggle = true;
   checked = false;
   movecount = 0;
   whoTurn.textContent = "X";
   box.forEach(val => val.textContent = "")
+  X.textContent = "0";
+  O.textContent = "0";
 }
 
 function PassPlay(i) {
-  if (checked) {
-    if (box[i].textContent == "X" || box[i].textContent == "O") {
-      return;
-    }
-    if (toggle) {
-      box[i].textContent = "X";
-      whoTurn.textContent = "O";
-      toggle = !toggle;
-    } else {
-      box[i].textContent = "O";
-      whoTurn.textContent = "X";
-      toggle = !toggle;
-    }
-    setTimeout(() => {
-      winner();
-    }, 50);
+  if (box[i].textContent == "X" || box[i].textContent == "O") {
+    return;
   }
+  if (toggle) {
+    box[i].textContent = "X";
+    whoTurn.textContent = "O";
+    toggle = !toggle;
+  } else {
+    box[i].textContent = "O";
+    whoTurn.textContent = "X";
+    toggle = !toggle;
+  }
+  setTimeout(() => {
+    WhoIsWinner("X");
+  }, 50);
 }
 
 function computer() {
@@ -179,242 +80,118 @@ function vsComputer(val) {
       }
       movecount++;
     } else if (movecount == 1) {
-      checkWinner();
+      checkWin()
     }
     whoTurn.textContent = "Your Turn(X)"
   }, 500);
   setTimeout(() => {
-    winner();
+    WhoIsWinner();
   }, 500 + 50);
 }
 
-function checkWinner() {
-  if (
-    (box[0].textContent == "O" && box[1].textContent == "O" && box[2].textContent == "") ||
-    (box[0].textContent == "O" && box[2].textContent == "O" && box[1].textContent == "") ||
-    (box[2].textContent == "O" && box[1].textContent == "O" && box[0].textContent == "")
-  ) {
-    if (box[0].textContent == "") {
-      box[0].textContent = "O";
-    } else if (box[1].textContent == "") {
-      box[1].textContent = "O";
-    } else {
-      box[2].textContent = "O";
-    }
-  } else if (
-    (box[0].textContent == "O" && box[3].textContent == "O" && box[6].textContent == "") ||
-    (box[0].textContent == "O" && box[6].textContent == "O" && box[3].textContent == "") ||
-    (box[3].textContent == "O" && box[6].textContent == "O" && box[0].textContent == "")
-  ) {
-    if (box[0].textContent == "") {
-      box[0].textContent = "O";
-    } else if (box[3].textContent == "") {
-      box[3].textContent = "O";
-    } else {
-      box[6].textContent = "O";
-    }
-  } else if (
-    (box[0].textContent == "O" && box[4].textContent == "O" && box[8].textContent == "") ||
-    (box[0].textContent == "O" && box[8].textContent == "O" && box[4].textContent == "") ||
-    (box[4].textContent == "O" && box[8].textContent == "O" && box[0].textContent == "")
-  ) {
-    if (box[0].textContent == "") {
-      box[0].textContent = "O";
-    } else if (box[4].textContent == "") {
-      box[4].textContent = "O";
-    } else {
-      box[8].textContent = "O";
-    }
-  } else if (
-    (box[2].textContent == "O" && box[5].textContent == "O" && box[8].textContent == "") ||
-    (box[2].textContent == "O" && box[8].textContent == "O" && box[5].textContent == "") ||
-    (box[5].textContent == "O" && box[8].textContent == "O" && box[2].textContent == "")
-  ) {
-    if (box[2].textContent == "") {
-      box[2].textContent = "O";
-    } else if (box[5].textContent == "") {
-      box[5].textContent = "O";
-    } else {
-      box[8].textContent = "O";
-    }
-  }
-  else if (
-    (box[2].textContent == "O" && box[4].textContent == "O" && box[6].textContent == "") ||
-    (box[2].textContent == "O" && box[6].textContent == "O" && box[4].textContent == "") ||
-    (box[4].textContent == "O" && box[6].textContent == "O" && box[2].textContent == "")
-  ) {
-    if (box[2].textContent == "") {
-      box[2].textContent = "O";
-    } else if (box[4].textContent == "") {
-      box[4].textContent = "O";
-    } else {
-      box[6].textContent = "O";
-    }
-  }
-  else if (
-    (box[6].textContent == "O" && box[7].textContent == "O" && box[8].textContent == "") ||
-    (box[6].textContent == "O" && box[8].textContent == "O" && box[7].textContent == "") ||
-    (box[7].textContent == "O" && box[8].textContent == "O" && box[6].textContent == "")
-  ) {
-    if (box[6].textContent == "") {
-      box[6].textContent = "O";
-    } else if (box[7].textContent == "") {
-      box[7].textContent = "O";
-    } else {
-      box[8].textContent = "O";
-    }
-  }
-  else if (
-    (box[1].textContent == "O" && box[4].textContent == "O" && box[7].textContent == "") ||
-    (box[1].textContent == "O" && box[7].textContent == "O" && box[4].textContent == "") ||
-    (box[4].textContent == "O" && box[7].textContent == "O" && box[1].textContent == "")
-  ) {
-    if (box[1].textContent == "") {
-      box[1].textContent = "O";
-    } else if (box[4].textContent == "") {
-      box[4].textContent = "O";
-    } else {
-      box[7].textContent = "O";
-    }
-  }
-  else if (
-    (box[3].textContent == "O" && box[4].textContent == "O" && box[5].textContent == "") ||
-    (box[3].textContent == "O" && box[5].textContent == "O" && box[4].textContent == "") ||
-    (box[4].textContent == "O" && box[5].textContent == "O" && box[3].textContent == "")
-  ) {
-    if (box[3].textContent == "") {
-      box[3].textContent = "O";
-    } else if (box[4].textContent == "") {
-      box[4].textContent = "O";
-    } else {
-      box[5].textContent = "O";
-    }
-  }
-  else if (
-    (box[0].textContent == "X" && box[1].textContent == "X" && box[2].textContent == "") ||
-    (box[0].textContent == "X" && box[2].textContent == "X" && box[1].textContent == "") ||
-    (box[2].textContent == "X" && box[1].textContent == "X" && box[0].textContent == "")
-  ) {
-    if (box[0].textContent == "") {
-      box[0].textContent = "O";
-    } else if (box[1].textContent == "") {
-      box[1].textContent = "O";
-    } else {
-      box[2].textContent = "O";
-    }
+function draw() {
+  alert("It's a draw!");
+  reset();
+}
 
-  } else if (
-    (box[0].textContent == "X" && box[3].textContent == "X" && box[6].textContent == "") ||
-    (box[0].textContent == "X" && box[6].textContent == "X" && box[3].textContent == "") ||
-    (box[3].textContent == "X" && box[6].textContent == "X" && box[0].textContent == "")
-  ) {
-    if (box[0].textContent == "") {
-      box[0].textContent = "O";
-    } else if (box[3].textContent == "") {
-      box[3].textContent = "O";
-    } else {
-      box[6].textContent = "O";
-    }
-  } else if (
-    (box[0].textContent == "X" && box[4].textContent == "X" && box[8].textContent == "") ||
-    (box[0].textContent == "X" && box[8].textContent == "X" && box[4].textContent == "") ||
-    (box[4].textContent == "X" && box[8].textContent == "X" && box[0].textContent == "")
-  ) {
-    if (box[0].textContent == "") {
-      box[0].textContent = "O";
-    } else if (box[4].textContent == "") {
-      box[4].textContent = "O";
-    } else {
-      box[8].textContent = "O";
-    }
+function WhoIsWinner() {
+  const winningCombos = [
+    [0, 1, 2], // Top row
+    [3, 4, 5], // Middle row
+    [6, 7, 8], // Bottom row
+    [0, 3, 6], // Left column
+    [1, 4, 7], // Middle column
+    [2, 5, 8], // Right column
+    [0, 4, 8], // Diagonal from top-left to bottom-right
+    [2, 4, 6]  // Diagonal from top-right to bottom-left
+  ];
 
-  } else if (
-    (box[2].textContent == "X" && box[5].textContent == "X" && box[8].textContent == "") ||
-    (box[2].textContent == "X" && box[8].textContent == "X" && box[5].textContent == "") ||
-    (box[5].textContent == "X" && box[8].textContent == "X" && box[2].textContent == "")
-  ) {
-    if (box[2].textContent == "") {
-      box[2].textContent = "O";
-    } else if (box[5].textContent == "") {
-      box[5].textContent = "O";
-    } else {
-      box[8].textContent = "O";
+  for (let i = 0; i < winningCombos.length; i++) {
+    let [a, b, c] = winningCombos[i];
+    if (box[a].textContent == box[b].textContent && box[b].textContent == box[c].textContent && box[a].textContent != "") {
+      alert(`${box[a].textContent} wins!`);
+      if (box[a].textContent == "X") {
+        X.textContent = parseInt(X.textContent) + 1
+      } else {
+        O.textContent = parseInt(O.textContent) + 1
+      }
+      reset();
+      return;
     }
+  }
 
-  }
-  else if (
-    (box[2].textContent == "X" && box[4].textContent == "X" && box[6].textContent == "") ||
-    (box[2].textContent == "X" && box[6].textContent == "X" && box[4].textContent == "") ||
-    (box[4].textContent == "X" && box[6].textContent == "X" && box[2].textContent == "")
-  ) {
-    if (box[2].textContent == "") {
-      box[2].textContent = "O";
-    } else if (box[4].textContent == "") {
-      box[4].textContent = "O";
-    } else {
-      box[6].textContent = "O";
+  // Check if all boxes are filled (draw)
+  let isDraw = true;
+  for (let i = 0; i < box.length; i++) {
+    if (box[i].textContent == "") {
+      isDraw = false;
+      break;
     }
   }
-  else if (
-    (box[6].textContent == "X" && box[7].textContent == "X" && box[8].textContent == "") ||
-    (box[6].textContent == "X" && box[8].textContent == "X" && box[7].textContent == "") ||
-    (box[7].textContent == "X" && box[8].textContent == "X" && box[6].textContent == "")
-  ) {
-    if (box[6].textContent == "") {
-      box[6].textContent = "O";
-    } else if (box[7].textContent == "") {
-      box[7].textContent = "O";
-    } else {
-      box[8].textContent = "O";
+  if (isDraw) {
+    draw();
+  }
+}
+
+function checkWin() {
+  let excuted = false;
+  const winningCombos = [
+    [0, 1, 2], // Top row
+    [3, 4, 5], // Middle row
+    [6, 7, 8], // Bottom row
+    [0, 3, 6], // Left column
+    [1, 4, 7], // Middle column
+    [2, 5, 8], // Right column
+    [0, 4, 8], // Diagonal from top-left to bottom-right
+    [2, 4, 6]  // Diagonal from top-right to bottom-left
+  ];
+  for (let i = 0; i < winningCombos.length; i++) {
+    let WinProbality = winningCombos[i];
+    if ((box[WinProbality[0]].textContent == "O" && box[WinProbality[1]].textContent == "O" && box[WinProbality[2]].textContent == "") ||
+      (box[WinProbality[0]].textContent == "" && box[WinProbality[1]].textContent == "O" && box[WinProbality[2]].textContent == "O") ||
+      (box[WinProbality[0]].textContent == "O" && box[WinProbality[1]].textContent == "" && box[WinProbality[2]].textContent == "O")) {
+      for (let i = 0; i < WinProbality.length; i++) {
+        if (box[WinProbality[i]].textContent == "") {
+          box[WinProbality[i]].textContent = "O"
+        }
+      };
+      excuted = true
+      break;
     }
   }
-  else if (
-    (box[1].textContent == "X" && box[4].textContent == "X" && box[7].textContent == "") ||
-    (box[1].textContent == "X" && box[7].textContent == "X" && box[4].textContent == "") ||
-    (box[4].textContent == "X" && box[7].textContent == "X" && box[1].textContent == "")
-  ) {
-    if (box[1].textContent == "") {
+  if (!excuted) {
+    for (let i = 0; i < winningCombos.length; i++) {
+      let WinProbality = winningCombos[i];
+      if ((box[WinProbality[0]].textContent == "X" && box[WinProbality[1]].textContent == "X" && box[WinProbality[2]].textContent == "") ||
+        (box[WinProbality[0]].textContent == "" && box[WinProbality[1]].textContent == "X" && box[WinProbality[2]].textContent == "X") ||
+        (box[WinProbality[0]].textContent == "X" && box[WinProbality[1]].textContent == "" && box[WinProbality[2]].textContent == "X")) {
+        for (let i = 0; i < WinProbality.length; i++) {
+          if (box[WinProbality[i]].textContent == "") {
+            box[WinProbality[i]].textContent = "O"
+          }
+        };
+        excuted = true
+        break;
+      }
+    }
+  }
+  if (!excuted) {
+    let random = Math.random()
+    if (random < 0.25 && box[1].textContent == "") {
       box[1].textContent = "O";
-    } else if (box[4].textContent == "") {
-      box[4].textContent = "O";
-    } else {
-      box[7].textContent = "O";
-    }
-  }
-  else if (
-    (box[3].textContent == "X" && box[4].textContent == "X" && box[5].textContent == "") ||
-    (box[3].textContent == "X" && box[5].textContent == "X" && box[4].textContent == "") ||
-    (box[4].textContent == "X" && box[5].textContent == "X" && box[3].textContent == "")
-  ) {
-    if (box[3].textContent == "") {
+    } else if (random > 0.25 && random < 0.5 && box[3].textContent == "") {
       box[3].textContent = "O";
-    } else if (box[4].textContent == "") {
-      box[4].textContent = "O";
-    } else {
+    } else if (random > 0.5 && random < 0.75 && box[5].textContent == "") {
       box[5].textContent = "O";
-    }
-  } else {
-    let ran = Math.random()
-    if (ran < 0.25 && box[5].textContent == "") {
-      box[5].textContent = "O"
-    }
-    else if (ran < 0.5 && ran > 0.25 && box[3].textContent == "") {
-      box[3].textContent = "O"
-    }
-    else if (ran < 0.75 && ran > 0.5 && box[7].textContent == "") {
-      box[7].textContent = "O"
-    }
-    else if (ran < 1 && ran > 0.75 && box[1].textContent == "") {
-      box[1].textContent = "O"
+    } else if (random > 0.75 && random < 1 && box[7].textContent == "") {
+      box[7].textContent = "O";
     } else {
-      if (box[5].textContent == "") {
-        box[5].textContent = "O"
-      } else if (box[3].textContent == "") {
-        box[3].textContent == "O"
-      } else if (box[7].textContent == "") {
-        box[7].textContent == "O"
-      } else if (box[1].textContent == "") {
-        box[7].textContent == "O"
+      for (let i = 0; i < box.length; i++) {
+        const element = box[i];
+        if (element.textContent == "") {
+          element.textContent = "O";
+          break;
+        }
       }
     }
   }
